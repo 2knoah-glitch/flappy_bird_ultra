85873f205ccf299c3ad99c44f9a097cc766f9cf3
ABYSSCORE <dontbannedme96@gmail.com>
2026-07-15 19:50:35 -0400

    Create ScoreDisplay component with styled text and black outline

--- lib/game/score_display.dart
+++ lib/game/score_display.dart
@@ -1,7 +1,37 @@
-// score_display.dart
+import 'dart:ui';
+import 'package:flame/components.dart';
+import 'package:flutter/material.dart';
+import 'flapping_bird_game.dart';
 
-class ScoreDisplay {
-  int score = 0;
+/// Big white score text with a black outline, displayed top-center.
+class ScoreDisplay extends TextComponent with HasGameRef<FlappingBirdGame> {
+  @override
+  Future<void> onLoad() async {
+    textRenderer = TextPaint(
+      style: const TextStyle(
+        fontSize: 64,
+        color: Colors.white,
+        fontWeight: FontWeight.w800,
+        fontFamily: 'Roboto',
+        shadows: [
+          Shadow(offset: Offset(-3, 0), color: Colors.black),
+          Shadow(offset: Offset(3, 0), color: Colors.black),
+          Shadow(offset: Offset(0, -3), color: Colors.black),
+          Shadow(offset: Offset(0, 3), color: Colors.black),
+          Shadow(offset: Offset(-2, -2), color: Colors.black),
+          Shadow(offset: Offset(2, -2), color: Colors.black),
+          Shadow(offset: Offset(-2, 2), color: Colors.black),
+          Shadow(offset: Offset(2, 2), color: Colors.black),
+        ],
+      ),
+    );
+    anchor = Anchor.topCenter;
+  }
 
-  String formatted() => score.toString();
+  @override
+  void update(double dt) {
+    text = gameRef.score.toString();
+    x = gameRef.size.x / 2;
+    y = 40;
+  }
 }
