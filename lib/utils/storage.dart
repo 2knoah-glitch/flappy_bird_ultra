5c7b03dba2893f035b8a2f5be35a219844ab72da
ABYSSCORE <dontbannedme96@gmail.com>
2026-07-15 19:25:44 -0400

    Add lib/utils/storage.dart stub

---
+++ lib/utils/storage.dart
@@ -0,0 +1,13 @@
+// utils/storage.dart
+
+class Storage {
+  // Placeholder for persistent storage (high score, settings)
+  Future<int> getHighScore() async {
+    // TODO: implement using shared_preferences
+    return 0;
+  }
+
+  Future<void> setHighScore(int score) async {
+    // TODO: implement
+  }
+}
